c901bb34fafb03971867292861e12003c7fa841d
maryam329 <maryamshaikh800@gmail.com>
2026-01-06 16:20:24 +0500

    Time: 0 ms (100.00%) | Memory: 43.7 MB (8.89%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0; // position to place next non-val element
+        int k = 0; 
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
