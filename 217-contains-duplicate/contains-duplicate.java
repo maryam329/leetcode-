603e51c4f25e3318040e0496bbdf62b788b28af4
maryam329 <maryamshaikh800@gmail.com>
2026-01-06 16:18:26 +0500

    Time: 10 ms (99.19%) | Memory: 98.2 MB (7.43%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        java.util.HashSet<Integer> set = new java.util.HashSet<>();
+
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true; // duplicate found
+            }
+        }
+
+        return false;
+    }
+}
